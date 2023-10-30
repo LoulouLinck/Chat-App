a237b28693db20ecd2cfe62882533a2648aa6970
Laure Lincker <136842839+LoulouLinck@users.noreply.github.com>
2023-10-30 18:41:15 +0100

    Updated App.js w/ 2 screens and navigation logic

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import the screens
+import StartScreen from './components/Start';
+import ChatScreen from './components/Chat';
 
-export default function App() {
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="StartScreen"
+      >
+        <Stack.Screen
+          name="Start"
+          component={StartScreen}
+        />
+        <Stack.Screen
+          name="ChatScreen"
+          component={ChatScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +37,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
