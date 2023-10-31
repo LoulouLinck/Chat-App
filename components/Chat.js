b2c68afb8111585f816a771e3441e2b603698349
Laure Lincker <136842839+LoulouLinck@users.noreply.github.com>
2023-10-31 14:51:13 +0100

    Update Chat.js
    -A chat app needs to send, receive, and display messages, so it makes sense to add messages as a state.
    - Bellow state initialisation: add useEffect() function. It gets called right after the Chat component mounts.
    -Set the state w/ static msg to see each element of UI displayed right away: use setter method of the messages state, setMessages():
    -Messages follow format to work w/ Gifted Chat library: each msg requires an ID, a creation date, and a user object. The user object also requires a user ID, name, and avatar.

--- components/Chat.js
+++ components/Chat.js
@@ -1,9 +1,29 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat } from "react-native-gifted-chat";
 
 const ChatScreen = ({ route, navigation }) => {
     const username = route.params.name;
     const color = route.params.color;
+    const [messages, setMessages] = useState([]);
+    const onSend = (newMessages) => {
+      setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    }
+
+   useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
 
   useEffect(() => {
     navigation.setOptions({ title: username });
@@ -12,6 +32,15 @@ const ChatScreen = ({ route, navigation }) => {
  return (
    <View style={[styles.container, {backgroundColor: color}]}>
      <Text>Hello {username} !</Text>
+     <View>
+        <GiftedChat
+          messages={messages}
+          onSend={messages => onSend(messages)}
+          user={{
+            _id: 1
+          }}
+        />
+    </View>
    </View>
  );
 }
@@ -19,8 +48,8 @@ const ChatScreen = ({ route, navigation }) => {
 const styles = StyleSheet.create({
  container: {
    flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center'
+  //  justifyContent: 'center',
+  //  alignItems: 'center'
  }
 });
 
