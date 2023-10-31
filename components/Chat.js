22a20b76535d6e45557f493b54f01306fb6b8b0c
Laure Lincker <136842839+LoulouLinck@users.noreply.github.com>
2023-10-31 17:44:38 +0100

    reorganised code

--- components/Chat.js
+++ components/Chat.js
@@ -1,20 +1,15 @@
 import { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
-import { GiftedChat } from "react-native-gifted-chat";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const ChatScreen = ({ route, navigation }) => {
     const username = route.params.name;
     const color = route.params.color;
     // messages state initialization
     const [messages, setMessages] = useState([]);
-    // setter function setMessage() accepts callback function which 1st parameter: previousMessages = variable refers to latest value of state
-    //appends the new msg to newMessages array: to original list of msg from previousMessages. Gets displayed in chat.
-    const onSend = (newMessages) => {
-      setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
-    }
 
-    // State initialization
-   useEffect(() => { 
+    useEffect(() => {
+      navigation.setOptions({ title: username })
     // Msg w/ Gifted Chat follow format: ID, creation date, user object. User object requires: user ID, name, avatar.
     setMessages([
       {
@@ -30,9 +25,6 @@ const ChatScreen = ({ route, navigation }) => {
     ]);
   }, []);
 
-  useEffect(() => {
-    navigation.setOptions({ title: username });
-  }, []);
 
  return (
    <View style={[styles.container, {backgroundColor: color}]}>
