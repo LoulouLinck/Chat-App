bef937ef2c3b690b34cb1a707b0d1617f81371de
Laure Lincker <136842839+LoulouLinck@users.noreply.github.com>
2023-11-07 21:56:31 +0100

    asks access permission and returns picked file
    asynchronous function that returns an object containing the media file data

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -8,5 +8,18 @@
           if (!result.canceled) setImage(result.assets[0]);
           else setImage(null)
         }
-      }
+    }
+    // lets user pick an image from library
+    const takePhoto = async () => {
+        let permissions = await ImagePicker.requestCameraPermissionsAsync();
+    
+        if (permissions?.granted) {
+          let result = await ImagePicker.launchCameraAsync();
+    
+         if (!result.canceled) setImage(result.assets[0]);
+        else setImage(null)
+        }
+    }
+    
+  
   
