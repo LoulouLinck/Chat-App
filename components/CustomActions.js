dbafc17c05017ffbd0ec1188b030010455c7b457
Laure Lincker <136842839+LoulouLinck@users.noreply.github.com>
2023-11-07 21:25:46 +0100

    grants permission to access library and lets users pick a file
    function that lets the user pick an image from the library:
    
    -asks user for permission: using ImagePicker.launchImageLibraryAsync API. If so:
    
    -function returns an object that has many properties, referenced by the permissions variable. The property you want to check from the returned object is .granted, which is a boolean.
    -After user has granted you access to media library (permissions?.granted is true), call ImagePicker.launchImageLibraryAsync() to let them pick a file.

---
+++ components/CustomActions.js
@@ -0,0 +1,12 @@
+    // Lets the user pick an image from the library
+    const pickImage = async () => {
+        let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    
+        if (permissions?.granted) {
+           let result = await ImagePicker.launchImageLibraryAsync();
+    
+          if (!result.canceled) setImage(result.assets[0]);
+          else setImage(null)
+        }
+      }
+  
